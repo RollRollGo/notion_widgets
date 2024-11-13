54d525cb00ef673b63352ab3fb62cced07290257
Xu Quan <xuquan1225@hotmail.com>
2024-11-13 22:00:25 +0800

    refactor: update window title via useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
-import './App.css';
+import { useEffect } from "react";
+import "./App.css";
 
 const App = () => {
+  useEffect(() => {
+    document.title = "樱桃小丸子语录";
+  }, []);
+
   return (
     <div className="content">
       <h1>Rsbuild with React</h1>
