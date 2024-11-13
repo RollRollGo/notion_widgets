36622f4ab3cdd0803a85272bca0532f3d10d647a
Xu Quan <xuquan1225@hotmail.com>
2024-11-13 22:01:30 +0800

    refactor: migrate html content to App component

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,24 @@ const App = () => {
   }, []);
 
   return (
-    <div className="content">
-      <h1>Rsbuild with React</h1>
-      <p>Start building amazing things with Rsbuild.</p>
+    <div id="background">
+      <div id="container">
+        <div id="quote"></div>
+        <div id="author">——《樱桃小丸子》</div>
+        <div id="divider"></div>
+        <div id="footer">
+          <img id="logo" src="../assets/logo_figure.png" />
+          <span id="info">
+            Powered by
+            <a
+              href="https://www.notion.so/roll-roll-go/s-Home-1beed66eeff84a3cabc455bee81c7237"
+              target="_blank"
+            >
+              RollRollGo
+            </a>
+          </span>
+        </div>
+      </div>
     </div>
   );
 };
